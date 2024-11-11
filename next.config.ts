02ec5020436fc30eab75beb9ff9590298d3c37c4
umylive <umylive@msn.com>
2024-11-12 02:54:34 +0300

    Fix dependency versions

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig = {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
+    reactStrictMode: true,
+    swcMinify: true,
   },
 };
 
